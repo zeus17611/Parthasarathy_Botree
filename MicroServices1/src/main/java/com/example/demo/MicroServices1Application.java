225e7df0cc3e1250046328a90bf7a04763613b5c
zeus17611 <74307832+zeus17611@users.noreply.github.com>
2022-03-31 10:57:49 +0530

    Add files via upload

---
+++ MicroServices1/src/main/java/com/example/demo/MicroServices1Application.java
@@ -0,0 +1,9 @@
+package com.example.demo;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class MicroServices1Application {
+	public static void main(String[] args) {
+		SpringApplication.run(MicroServices1Application.class, args);
+	}
+}
---
+++ jfsMicroService/src/main/java/com/example/demo/JfsMicroServiceApplication.java
@@ -0,0 +1,9 @@
+package com.example.demo;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class JfsMicroServiceApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(JfsMicroServiceApplication.class, args);
+	}
+}
